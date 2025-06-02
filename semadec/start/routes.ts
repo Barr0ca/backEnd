cd59d42e8ba416d09ccded49683a582171814427
Barr0ca <iandearaujogalvao@gmail.com>
2025-06-02 18:13:36 -0300

    Criando a rota para 'indexById'

--- semadec/start/routes.ts
+++ semadec/start/routes.ts
@@ -10,6 +10,7 @@
 import router from '@adonisjs/core/services/router'
 
 router.get('/', '#controllers/equipes_controller.index')
+router.get('/:id', '#controllers/equipes_controller.indexById')
 router.post('/', '#controllers/equipes_controller.create')
 router.put('/:id', '#controllers/equipes_controller.update')
 router.delete('/:id', '#controllers/equipes_controller.delete')
